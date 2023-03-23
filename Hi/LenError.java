0492de4685cc30f753633da93b1a692f97d8e08d
sreylenchab <126034400+CHAB-Sreylen@users.noreply.github.com>
2023-03-23 17:11:22 +0700

    Update LenError.java

--- Hi/LenError.java
+++ Hi/LenError.java
@@ -3,5 +3,6 @@ package Hi;
 public class LenError {
     public static void main(String[] args) {
         System.out.println("Len error");
+        System.out.println("I am CHAB Sreylen");
     }
 }
