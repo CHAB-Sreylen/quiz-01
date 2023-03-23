e3471c85b7bfcef580720a05eadd79b3a55da233
ANG-OUSA <126034407+ANG-OUSA@users.noreply.github.com>
2023-03-23 17:14:26 +0700

    Update LenError.java

--- Hi/LenError.java
+++ Hi/LenError.java
@@ -3,5 +3,6 @@ package Hi;
 public class LenError {
     public static void main(String[] args) {
         System.out.println("Len error");
+        System.out.println("Hi can you hear me");
     }
 }
